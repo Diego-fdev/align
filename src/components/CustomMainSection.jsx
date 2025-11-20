93dc3076f3c3d30168191e2b69422937384c40e8
Diego Aldao <diegofrontdev@gmail.com>
2025-11-20 15:07:39 -0300

    añadido un componente reutilizable para las secciones principales

---
+++ src/components/CustomMainSection.jsx
@@ -0,0 +1,9 @@
+export default function CustomMainSection({ children, customStyles }) {
+  return (
+    <section
+      className={`py-10 lg:py-12 xl:py-14 px-4 md:px-8 lg:px-12 max-w-[1440px] w-full mx-auto 2xl:px-0 ${customStyles}`}
+    >
+      {children}
+    </section>
+  );
+}
