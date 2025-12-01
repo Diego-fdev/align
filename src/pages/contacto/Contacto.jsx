e9704854af6f6f9a4c84bada17887c96644b0e29
Diego Aldao <diegofrontdev@gmail.com>
2025-12-01 17:40:18 -0300

    añadida la pagina contacto

---
+++ src/pages/contacto/Contacto.jsx
@@ -0,0 +1,5 @@
+import SectionContacto from "../../components/common/SectionContacto";
+
+export default function Contacto() {
+  return <SectionContacto customStyles="pt-32 lg:pt-40 xl:pt-52" />;
+}
--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Route, Routes } from "react-router";
 import Inicio from "../pages/Inicio/Inicio";
 import MainLayout from "../layouts/MainLayout";
 import Tutoras from "../pages/tutoras/Tutoras";
+import Contacto from "../pages/contacto/Contacto";
 
 export default function AppRouter() {
   return (
@@ -10,6 +11,7 @@ export default function AppRouter() {
         <Route element={<MainLayout />}>
           <Route path="/" element={<Inicio />} />
           <Route path="/tutoras" element={<Tutoras />} />
+          <Route path="/contacto" element={<Contacto />} />
         </Route>
       </Routes>
     </BrowserRouter>
