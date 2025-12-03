4c3e39f3065edc037062d37220d42dd04c0cff2b
Diego Aldao <diegofrontdev@gmail.com>
2025-12-03 18:47:25 -0300

    añadidas las secciones faltantes

--- src/pages/servicios/Servicios.jsx
+++ src/pages/servicios/Servicios.jsx
@@ -1,9 +1,17 @@
+import SectionPreguntas from "../../components/common/preguntas/SectionPreguntas";
+import SectionContacto from "../../components/common/SectionContacto";
+import SectionDescripcion from "../Inicio/components/SectionDescripcion";
+import SectionDiscount from "./components/SectionDiscount";
 import SectionMainServices from "./components/SectionMainServices";
 
 export default function Servicios() {
   return (
     <>
       <SectionMainServices />
+      <SectionDescripcion />
+      <SectionDiscount />
+      <SectionContacto />
+      <SectionPreguntas />
     </>
   );
 }
