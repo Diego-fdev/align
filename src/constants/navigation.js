69b886295e24f405967a017ea32c87014b49508a
Diego Aldao <diegofrontdev@gmail.com>
2025-12-05 16:46:59 -0300

    añadida una constante para la navegacion del sitio

---
+++ src/constants/navigation.js
@@ -0,0 +1,7 @@
+export const navigationLinks = [
+  { id: 1, nombre: "entrenadoras", path: "/tutoras" },
+  { id: 2, nombre: "servicios", path: "/servicios" },
+  { id: 3, nombre: "blog", path: "/blog" },
+  { id: 4, nombre: "planes", path: "/planes" },
+  { id: 5, nombre: "contacto", path: "/contacto" },
+];
