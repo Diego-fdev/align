6be2a483c323d3268b848ee6ed71bc1c966f1e8f
Diego Aldao <diegofrontdev@gmail.com>
2025-12-08 14:06:07 -0300

    extraida la info a una constante

--- src/components/common/preguntas/SectionPreguntas.jsx
+++ src/components/common/preguntas/SectionPreguntas.jsx
@@ -1,46 +1,6 @@
+import { infoPreguntas } from "../../../constants/preguntas";
 import CustomMainSection from "../../CustomMainSection";
 import DropdownPregunta from "./DropdownPregunta";
-
-const infoPreguntas = [
-  {
-    id: 1,
-    pregunta: "Qué tipos de clases de yoga ofrecen?",
-    respuesta:
-      "Ofrecemos una variedad de estilos de yoga, incluidos Hatha, Vinyasa y Yin Yoga, adecuados para todos los niveles.",
-  },
-  {
-    id: 2,
-    pregunta:
-      "Necesito alguna experiencia previa para unirme a su programa de fitness?",
-    respuesta:
-      "Ofrecemos una variedad de estilos de yoga, incluidos Hatha, Vinyasa y Yin Yoga, adecuados para todos los niveles.",
-  },
-  {
-    id: 3,
-    pregunta: "Cómo reservo una clase de yoga personalizada?",
-    respuesta:
-      "Ofrecemos una variedad de estilos de yoga, incluidos Hatha, Vinyasa y Yin Yoga, adecuados para todos los niveles.",
-  },
-  {
-    id: 4,
-    pregunta: "Qué debo llevar a una clase de yoga?",
-    respuesta:
-      "Ofrecemos una variedad de estilos de yoga, incluidos Hatha, Vinyasa y Yin Yoga, adecuados para todos los niveles.",
-  },
-  {
-    id: 5,
-    pregunta:
-      "Sus planes nutricionales están adaptados a las necesidades individuales?",
-    respuesta:
-      "Ofrecemos una variedad de estilos de yoga, incluidos Hatha, Vinyasa y Yin Yoga, adecuados para todos los niveles.",
-  },
-  {
-    id: 6,
-    pregunta: "Cuánto duran los talleres de bienestar?",
-    respuesta:
-      "Ofrecemos una variedad de estilos de yoga, incluidos Hatha, Vinyasa y Yin Yoga, adecuados para todos los niveles.",
-  },
-];
 
 export default function SectionPreguntas() {
   return (
