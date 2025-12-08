3de576592745bd73047ebb77db6159674d45e420
Diego Aldao <diegofrontdev@gmail.com>
2025-12-08 14:12:09 -0300

    añadida la seccion preguntas

--- src/pages/contacto/Contacto.jsx
+++ src/pages/contacto/Contacto.jsx
@@ -1,5 +1,11 @@
+import SectionPreguntas from "../../components/common/preguntas/SectionPreguntas";
 import SectionContacto from "../../components/common/SectionContacto";
 
 export default function Contacto() {
-  return <SectionContacto customStyles="pt-32 lg:pt-40 xl:pt-52" />;
+  return (
+    <>
+      <SectionContacto customStyles="pt-32 lg:pt-40 xl:pt-52" />
+      <SectionPreguntas />
+    </>
+  );
 }
