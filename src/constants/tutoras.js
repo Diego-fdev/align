7cb82f858e3345ab417ed530109c7bc629c6c2b5
Diego Aldao <diegofrontdev@gmail.com>
2025-12-08 14:09:44 -0300

    extraida la info a una constante

---
+++ src/constants/tutoras.js
@@ -0,0 +1,36 @@
+import imagenMiembroEquipoUno from "../assets/equipo/miembro-1.webp";
+import imagenMiembroEquipoDos from "../assets/equipo/miembro-2.webp";
+import imagenMiembroEquipoTres from "../assets/equipo/miembro-1.webp";
+
+export const infoTutoras = [
+  {
+    id: 1,
+    nombre: "claire ford",
+    imagenSrc: imagenMiembroEquipoUno,
+  },
+  {
+    id: 2,
+    nombre: "alicia regis",
+    imagenSrc: imagenMiembroEquipoDos,
+  },
+  {
+    id: 3,
+    nombre: "lena parker",
+    imagenSrc: imagenMiembroEquipoTres,
+  },
+  {
+    id: 4,
+    nombre: "claire ford",
+    imagenSrc: imagenMiembroEquipoUno,
+  },
+  {
+    id: 5,
+    nombre: "alicia regis",
+    imagenSrc: imagenMiembroEquipoDos,
+  },
+  {
+    id: 6,
+    nombre: "lena parker",
+    imagenSrc: imagenMiembroEquipoTres,
+  },
+];
