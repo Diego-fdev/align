7c53a079f5d9b0ca02e393b75330cf2cb9aa3c12
Diego Aldao <diegofrontdev@gmail.com>
2026-04-02 22:19:00 -0300

    añadido el componente SupportLayout

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -6,6 +6,7 @@ import Contacto from "../pages/contacto/Contacto";
 import Planes from "../pages/planes/Planes";
 import Blog from "../pages/blog/Blog";
 import Servicios from "../pages/servicios/Servicios";
+import SupportLayout from "../layouts/SupportLayout";
 
 export default function AppRouter() {
   return (
@@ -13,11 +14,15 @@ export default function AppRouter() {
       <Routes>
         <Route element={<MainLayout />}>
           <Route path="/" element={<Inicio />} />
-          <Route path="/tutoras" element={<Tutoras />} />
           <Route path="/contacto" element={<Contacto />} />
-          <Route path="/planes" element={<Planes />} />
-          <Route path="/blog" element={<Blog />} />
-          <Route path="/servicios" element={<Servicios />} />
+        </Route>
+        <Route element={<MainLayout />}>
+          <Route element={<SupportLayout />}>
+            <Route path="/tutoras" element={<Tutoras />} />
+            <Route path="/planes" element={<Planes />} />
+            <Route path="/blog" element={<Blog />} />
+            <Route path="/servicios" element={<Servicios />} />
+          </Route>
         </Route>
       </Routes>
     </BrowserRouter>
